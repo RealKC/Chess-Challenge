9d63125b7a89f5747dda8196ada06da3cbdc41bc
Mițca Dumitru <dumitru0mitca@gmail.com>
2023-07-21 21:17:03 +0300

    Start working on the challenge
    Currently the AI will:
    - always try to engage in Queen's Gambit
    - do any move that checks the opponent or takes a piece
    - attempt to stall in the last 10 seconds of a game
    
    Co-authored-by: Mihalache Mihai <kime78.dev@gmail.com>

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,63 @@
-﻿using ChessChallenge.API;
+﻿using System.Linq;
+using System;
+using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
+    int queensGambit = 0;
+
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        var legalMoves = board.GetLegalMoves();
+        System.Random rng = new();
+
+        if (timer.MillisecondsRemaining < 10_000)
+        {
+            return legalMoves[rng.Next(legalMoves.Length)];
+        }
+
+        if (timer.MillisecondsRemaining < 5_000)
+        {
+            return legalMoves[0];
+        }
+
+        // queen's gambit is forced.
+        if (board.IsWhiteToMove)
+        {
+            if (queensGambit == 0)
+            {
+                Move move = new("d2d4", board);
+                if (legalMoves.Contains(move))
+                {
+                    return move;
+                }
+                else
+                {
+                    queensGambit = 69;
+                }
+            }
+            else if (queensGambit == 1)
+            {
+                return new("c2c4", board);
+            }
+        }
+
+        foreach (var move in legalMoves)
+        {
+            board.MakeMove(move);
+            if (board.IsInCheck())
+            {
+                board.UndoMove(move);
+                return move;
+            }
+            board.UndoMove(move);
+
+            if (move.IsCapture)
+            {
+                return move;
+            }
+        }
+
+        return legalMoves[rng.Next(legalMoves.Length)];
     }
 }
