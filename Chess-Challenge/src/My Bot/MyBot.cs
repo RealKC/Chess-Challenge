dd2f8b4f8d848c0786ea552d207d396d922c6a24
Kime <kime78.dev@gmail.com>
2023-07-22 17:29:22 +0300

    Implement the basic recursive function for move searching

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,123 +5,42 @@ using System.Collections.Generic;
 
 public class MyBot : IChessBot
 {
-    int queensGambit = 0;
     int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+    Move bestMove;
     public Move Think(Board board, Timer timer)
     {
-        var legalMoves = board.GetLegalMoves();
-        System.Random rng = new();
+        bestMove = board.GetLegalMoves()[1];
+        return Tumefiere(board, timer);
+    }
 
-        if (timer.MillisecondsRemaining < 10_000)
+    public Move Tumefiere(Board board, Timer timer, int depth = 0, bool enemy = false)
+    {
+        if(depth == 6)
         {
-            return legalMoves[rng.Next(legalMoves.Length)];
+            return bestMove;
         }
 
-        if (timer.MillisecondsRemaining < 5_000)
+        if(enemy)
         {
-            return legalMoves[0];
-        }
-
-        // queen's gambit is forced.
-        if (board.IsWhiteToMove)
-        {
-            if (queensGambit == 0)
+            var enemyMoves = board.GetLegalMoves();
+            foreach (var move in enemyMoves)
             {
-                Move move = new("d2d4", board);
-                if (legalMoves.Contains(move))
-                {
-                    return move;
-                }
-                else
-                {
-                    queensGambit = 69;
-                }
-            }
-            else if (queensGambit == 1)
-            {
-                return new("c2c4", board);
+                board.MakeMove(move);
+                return Tumefiere(board, timer, depth + 1, true);
+                board.UndoMove(move);
             }
         }
-
-        foreach (var move in legalMoves)
+        else
         {
-            if (move.ToString().EndsWith("q")) //force the promotions to be queens
-                return move;
-            if(MoveIsCheckmate(board, move))
-                return move;
-            if(MoveIsSafeCheck(board, move))
-                return move;
-            if(MoveIsUnsafeCheck(board, move)) 
-                return move;
-            if (MoveIsSafeCapture(board, move))
-                return move;
-            //if (MoveIsInSafeSquare(board, move)) //very bad, leads to repetition
-            //    return move;
+            var ourMoves = board.GetLegalMoves();
+            foreach(var move in ourMoves)
+            {
+                board.MakeMove(move);
+                return Tumefiere(board, timer, depth + 1, true);
+                board.UndoMove(move);
+            }
         }
-
-        return legalMoves[rng.Next(legalMoves.Length)];
+        return bestMove;
     }
 
-    bool MoveIsCheckmate(Board board, Move move)
-    {
-        board.MakeMove(move);
-        bool isMate = board.IsInCheckmate();
-        board.UndoMove(move);
-        return isMate;
-    }
-
-    bool MoveIsUnsafeCheck(Board board, Move move)
-    {
-        board.MakeMove(move);
-        bool isCheck = board.IsInCheck();
-        board.UndoMove(move);
-        return isCheck;
-    }
-    bool MoveIsSafeCheck(Board board, Move move) //safe means that a piece can't take our piece in the new position
-    {
-        if (MoveIsUnsafeCheck(board, move))
-        {
-            Square targetSquare = move.TargetSquare;
-            board.MakeMove(move);
-            Move[] enemyMoves = board.GetLegalMoves();
-            board.UndoMove(move);
-            foreach (Move enemyMove in enemyMoves)
-                if (enemyMove.TargetSquare == targetSquare)
-                    return false;
-
-            return true;
-        }
-
-        return false;
-    }
-    /*bool MoveIsInSafeSquare(Board board, Move move)
-    {
-        Square targetSquare = move.TargetSquare;
-        board.MakeMove(move);
-        Move[] enemyMoves = board.GetLegalMoves();
-        board.UndoMove(move);
-        foreach (Move enemyMove in enemyMoves)
-            if (enemyMove.TargetSquare == targetSquare)
-                return false;
-
-        return true;
-    }*/
-
-    bool MoveIsSafeCapture(Board board, Move move) //safe means that a piece can't take our piece in the new position
-    {
-        if (move.IsCapture)
-        {
-            Square targetSquare = move.TargetSquare;
-            board.MakeMove(move);
-            Move[] enemyMoves = board.GetLegalMoves();
-            board.UndoMove(move);
-            foreach (Move enemyMove in enemyMoves)  
-                if(enemyMove.TargetSquare == targetSquare) 
-                    return false;
-         
-            return true;
-        }
-            
-        return false;
-    }
 }
